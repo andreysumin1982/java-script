44bb83348196dc99fe3fa5667e2bf20e6190c74d
asumin1982 <s_andriano@mail.ru>
2021-12-08 16:49:59 +0300

    Добавлены ф-ция getTasks

--- work10/js/service.js
+++ work10/js/service.js
@@ -1,1 +1,13 @@
 /*Создание ajax-запросов GET/POST, промисы */
+//
+//
+let urlGetTasks = 'https://jsonplaceholder.typicode.com/posts';
+//
+//Создаем ф-цию getTasks. Показываем таски
+function getTasks(){
+    const promise = axios.get(urlGetTasks);
+        promise.then((response)=>{
+            //console.log(response)
+            return response.data
+        })
+}; 
