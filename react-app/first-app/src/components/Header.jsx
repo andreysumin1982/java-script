259920fdad7552c2c26faf1cc7ab88a43cbe40cc
asumin1982 <s_andriano@mail.ru>
2022-03-15 15:18:13 +0300

    Разбиваем проект на компоненты. Папка для компонент

---
+++ react-app/first-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+// Создаем компоненту Header
+const Header = () =>{
+    return (
+        <header className='header'>
+            Header
+        </header>
+    );
+};
+//
+export default Header;
---
+++ react-app/first-app/src/components/Nav.jsx
@@ -0,0 +1,24 @@
+// Создаем компоненту Nav
+const Nav = () => {
+    return (
+        <nav className='nav'>
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>
+                <a>Messages</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+            <div>
+                <a>Setting</a>
+            </div>
+        </nav>
+    );
+};
+//
+export default Nav;
