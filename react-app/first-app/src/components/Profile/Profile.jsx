2f1c00e02c628de6484b6c09cd628149239dd076
asumin1982 <s_andriano@mail.ru>
2022-03-16 15:20:08 +0300

    Рефакторинг

--- react-app/first-app/src/components/Profile/Profile.jsx
+++ react-app/first-app/src/components/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import MyPosts from "./MyPosts/MyPosts";
 import p from"./Profile.module.css";
 
 // Создаем компоненту Profile
@@ -5,27 +6,13 @@ const Profile = () => {
     return (
         <div className={p.content}>
             <div>
-                <img src='https://img3.goodfon.ru/original/1920x1080/c/80/valentina-litso-volosy-figura-sportzal-zal-sportivnyi-stil-1.jpg'></img>
+                <img className={p.contentImg} src='https://img3.goodfon.ru/original/1920x1080/c/80/valentina-litso-volosy-figura-sportzal-zal-sportivnyi-stil-1.jpg'></img>
             </div>
             <div>
                 Ава + description
             </div>
 
-            <div>
-                Мои посты
-                <div>
-                    Новые посты
-                </div>
-                <div className="posts">
-                    <div className={p.item}>
-                        post1
-                    </div>
-                    <div>
-                        post2
-                    </div>
-                </div>
-            </div>
-
+            <MyPosts />
         </div>
     );
 };
