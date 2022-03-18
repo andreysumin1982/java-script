5d40986722f6436afe3b2b7d2f8230ab7bfc6133
asumin1982 <s_andriano@mail.ru>
2022-03-18 10:26:50 +0300

    Создана компонента Dialogs

---
+++ react-app/first-app/src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import d from"./Dialogs.module.css"; // Импортируем модуль css
+
+// Создаем компоненту Dialogs
+const Dialogs = () => {
+    return (
+        <div className={d.content}>
+          *  Dialogs *
+        </div>
+    );
+};
+//
+export default Dialogs;
