2c6dc03d9708d6759b5ad9baf93c9eef4fc3336f
asumin1982 <s_andriano@mail.ru>
2022-03-18 10:30:27 +0300

    Установлен пкет react-router-dom, Profile и Messages маршрутизируются. Произведен рфакторинг кода

--- react-app/first-app/src/App.js
+++ react-app/first-app/src/App.js
@@ -3,15 +3,28 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import Nav from './components/Nav/Nav.jsx';
 import Profile from './components/Profile/Profile.jsx';
+import Dialogs from './components/Dialogs/Dialogs';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+//
 function App() {
-  // Верска GRID в одной компоненте 
+  //
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Nav />
+
+        <div>
+          <Routes>
+            <Route path="/profile" element = {<Profile/>}/>
+            <Route path="/dialogs" element = {<Dialogs/>}/>  
+          </Routes>
+        </div>
+
+      </div>
+    </BrowserRouter>
+
   );
 }
 //
