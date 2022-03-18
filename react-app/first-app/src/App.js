5126052beded6f3b6b35ac14c400d293cb5f8dd0
asumin1982 <s_andriano@mail.ru>
2022-03-18 11:50:32 +0300

    Созданы компоненты News, Settings, Music, изменены стили, добавлены маршруты

--- react-app/first-app/src/App.js
+++ react-app/first-app/src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 import Header from './components/Header/Header.jsx';
 import Nav from './components/Nav/Nav.jsx';
 import Profile from './components/Profile/Profile.jsx';
-import Dialogs from './components/Dialogs/Dialogs';
+import Dialogs from './components/Dialogs/Dialogs.jsx';
+import News from './components/News/News.jsx';
+import Music from './components/Music/Music.jsx';
+import Settings from './components/Settings/Settings';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 //
@@ -15,16 +18,18 @@ function App() {
         <Header />
         <Nav />
 
-        <div>
-          <Routes>
+        <div> 
+          <Routes>  {/* Подключаем маршруты */}
             <Route path="/profile" element = {<Profile/>}/>
-            <Route path="/dialogs" element = {<Dialogs/>}/>  
+            <Route path="/dialogs" element = {<Dialogs/>}/>
+            <Route path="/news" element = {<News/>}/>
+            <Route path="/music" element = {<Music/>}/>
+            <Route path="/settings" element = {<Settings/>}/>
           </Routes>
         </div>
 
       </div>
     </BrowserRouter>
-
   );
 }
 //
