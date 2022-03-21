9d92ad1e3caaac0050434e8abd166c3ff57272e0
asumin1982 <s_andriano@mail.ru>
2022-03-21 11:49:19 +0300

    Добавлены стили по умолчанию, NavLink

--- react-app/first-app/src/components/Nav/Nav.jsx
+++ react-app/first-app/src/components/Nav/Nav.jsx
@@ -1,24 +1,26 @@
 import n from "./Nav.module.css";
 import defaultStyle from '../../DefaultStyle/DefaultStyle.module.css'
+// NavLink - роутинг без перезагрузки страницы
+import { NavLink } from "react-router-dom";
 
 // Создаем компоненту Nav
 const Nav = () => {
     return (
         <nav className={`${defaultStyle.defaultStyle} ${n.nav}`}>
             <div className={n.item}>
-                <a className={n.links} href="/profile">Profile</a>
+                <NavLink className={n.links} to="/profile">Profile</NavLink>
             </div>
             <div className={n.item}>
-                <a className={n.links} href="/dialogs">Messages</a>
+                <NavLink className={n.links} to="/dialogs">Messages</NavLink>
             </div>
             <div className={n.item}>
-                <a className={n.links} href="/news">News</a>
+                <NavLink className={n.links} to="/news">News</NavLink>
             </div>
             <div className={n.item}>
-                <a className={n.links} href="/music">Music</a>
+                <NavLink className={n.links} to="/music">Music</NavLink>
             </div>
             <div className={n.item}>
-                <a className={n.links} href="/settings">Setting</a>
+                <NavLink className={n.links} to="/settings">Setting</NavLink>
             </div>
         </nav>
     );
