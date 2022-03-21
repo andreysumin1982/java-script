c8df19d0428a6e5ebe486da128f52ed7de2f53c6
asumin1982 <s_andriano@mail.ru>
2022-03-21 16:33:40 +0300

    Верстаем компоненту Dialogs

--- react-app/first-app/src/components/Dialogs/Dialogs.jsx
+++ react-app/first-app/src/components/Dialogs/Dialogs.jsx
@@ -5,9 +5,34 @@ import defaultStyle from '../../DefaultStyle/DefaultStyle.module.css'
 const Dialogs = () => {
     return (
           // Использование нескольких классов css
-        <div className={`${defaultStyle.defaultStyle} ${d.content}`}> 
-          *  Dialogs *
+      <div className={`${defaultStyle.defaultStyle} ${d.dialogs}`}>
+        <div className="dialogs_item">
+          <div className="dialog">
+            *  Dialogs1 *
+          </div>
+          <div className="dialog">
+            *  Dialogs2 *
+          </div>
+          <div className="dialog">
+            *  Dialogs3 *
+          </div>
+          <div className="dialog">
+            *  Dialogs4 *
+          </div>
+          <div className="dialog">
+            *  Dialogs5 *
+          </div>
         </div>
+        <div className="messages">
+          <div className="message">Hello</div>
+          <div className="message">By</div>
+          <div className="message">Go</div>
+          <div className="message">de</div>
+          <div className="message">Br</div>
+        </div>
+      </div>
+      
+
     );
 };
 //
