856a476c898925b7686546c88986aca1725c37c8
asumin1982 <s_andriano@mail.ru>
2022-03-24 16:36:22 +0300

    Выносим получ.данные с сервера за пределы UI

--- react-app/first-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-app/first-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -12,12 +12,12 @@ const MyPosts = (props) => {
         {id : 4, message: "Объект Console служит для доступа к средствам отладки браузера (например, Web Console (Веб-консоль) в Firefox). Работа с ним отличается в разных браузерах, но эти методы и свойства по факту поддерживаются всеми браузерами."},
         {id : 5, message: "Объект Console служит для доступа к средствам отладки браузера (например, Web Console (Веб-консоль) в Firefox). Работа с ним отличается в разных браузерах, но эти методы и свойства по факту поддерживаются всеми браузерами."}
     ]
-    // Перебираем массив объектов получ. с сервака и вытаскиваем данн. в новый массив 
+    //Перебираем массив объектов получ. с сервака и вытаскиваем данн. в новый массив 
     let postsElements = postsData.map(p => <Post message = {p.message}/>)
     //
     return (
         <div className={`${defaultStyle.defaultStyle} ${mp.item}`}>
-            Мои посты
+            * Мои посты *
             <div>
                 <textarea></textarea>
                 <button>Add</button>
@@ -25,6 +25,6 @@ const MyPosts = (props) => {
                 {postsElements}
         </div>
     );
-}
+};
 //
 export default MyPosts;
--- react-app/first-app/src/components/Profile/MyPosts/Post/Post.jsx
+++ react-app/first-app/src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import Like from "../Like/Like.jsx"; // Подключаем лайки
-import post from "./Post.module.css";
+import p from "./Post.module.css";
 //
 const Post = (props) =>{ // Используем props
     return(
-        <div className={post.item}>
+        <div className={p.item}>
             <img src="https://demotivation.ru/wp-content/uploads/2020/11/53c0eb8812a6bc6461377c31502ae040.jpg"></img>
-                {props.message}
-                
-                <Like />
+            {props.message}
+
+            <Like />
         </div>
     );
 };
