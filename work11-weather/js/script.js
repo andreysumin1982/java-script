29f1bf57e520ffd06ddabae4f1b23af5950684d9
asumin1982 <s_andriano@mail.ru>
2022-05-11 14:07:18 +0300

    Рефакторинг кода

--- work11-weather/js/script.js
+++ work11-weather/js/script.js
@@ -1,28 +1,28 @@
-/* Events */
 //
-const url = 'https://wttr.in/'
-const url2 = 'https://jsonplaceholder.typicode.com/users'
-const url3 = 'https://news.ycombinator.com/'
-
+const url = 'https://wttr.in/%D0%A1%D0%B0%D0%BD%D0%BA%D1%82-%D0%9F%D0%B5%D1%82%D0%B5%D1%80%D0%B1%D1%83%D1%80%D0%B3';
 //
-datasetHTML = []
+function formatTime(){
+    //* Ф-ция возвр. дату и время
+    const date1 = new Date;
+    const d = date1.getDate().toString().padStart(2, "0");
+    const m = (date1.getMonth() + 1).toString().padStart(2, "0");
+    const y = date1.getFullYear().toString();
+    const h = date1.getHours().toString().padStart(2, "0");
+    const min = date1.getMinutes().toString().padStart(2, "0");
+    return `${d}-${m}-${y}, ${h}:${min}`;
+};
 //
-function make_url(city){
-    return `${url}${city}`
-}
-//
-function make_parameters(){
-    let params = {
-        'format': 2,  //погода одной строкой
-        'M': '',  // скорость ветра в "м/с"
-        'lang': 'ru'
-     }
-    return params
-}
+function handlerObj(outputData){
+    //* обработчик объекта HTMLDocument 
+    document.querySelector('.date').innerHTML = `Сегодня: ${formatTime()}`
+    document.querySelector('.name').innerHTML = `${outputData.body.innerHTML.slice(0,440)}`
+    document.querySelector('.description').innerHTML = `<pre>${outputData.body.innerHTML.slice(2730,5500)}</pre>`
+    //console.log(outputData.body.innerHTML.slice(0,5500))
+};
 //
 function parser(){
     //
-   fetch(make_url('Saint Petersburg'), make_parameters())
+   fetch(url) //
         .then((response) => { // Возвр. промис
             //console.log(response)
             document.querySelector('.temp').innerHTML = `Status: ${response.status}<br>
@@ -32,67 +32,28 @@ function parser(){
             response.text() // Возвр. промис
                 .then(data => 
                     {
-                        function getDate(){
-                            let date1 = new Date
-                                d = String(date1.getDate())
-                                m = String(date1.getMonth() + 1)
-                                y = String(date1.getFullYear())
-                            return d+'-'+m+'-'+y;
-                        }
-                        //
                         let parser = new DOMParser();  
                         let doc = parser.parseFromString(data, "text/html");
-                        //console.log(doc.body.innerHTML.slice(0,450))         
-                        document.querySelector('.date').innerHTML = `Дата: ${getDate()} ${parseInt( Date.now()/1000)}`
-                        document.querySelector('.name').innerHTML = doc.body.innerHTML.slice(0,440)
+                        return handlerObj(doc)  // возвр. объект и пкредаем в ф-цию handlerObj
                     })
             })
-        .catch(error => {
+        .catch(error => { //error
             console.log(error)
             document.querySelector('.name').innerHTML = error
-        }); // fetch        
-};// parser
+        });     // fetch        
+};  // parser
 //
 let count = 0
 setInterval(() =>{
     /*
-        Обновляем погодные данные на странице
-        Выполняется кажд. 30 мин
+       * Обновляем погодные данные на странице
+       * Выполняется кажд. 30 мин
     */
         parser()
-        console.log(`Число запусков: ${count++}`);
+        console.log(`Кол-во запусков: ${++count}`);
     //
 
-},180000);
+},1800000);
 //
-parser()
-
-//---------------------------------------------------------------------------
-// Рекурсия
-function func(arr) {
-	for (let elem of arr) {
-		if (typeof elem == 'object') {
-			func(elem);
-		} else {
-			console.log(elem);
-		}
-	}
-}
+parser();
 //
-// Обход DOM дерева
-let parent = document.documentElement // html
-function recur(par){
-    if (par.children.length == 0){
-        console.log(par)
-        return
-    }
-    else {
-        for (let item of par.children){
-            if (item.children.length != 0){
-                recur(item)
-            }
-            else console.log(item)
-        }
-    }
-}
-//recur(parent)
